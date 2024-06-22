e7530cc87caa3c1d8ef35754710061fe56918b3d
Opeyemi Akande <opeyemi.akande@reventtechnologies.com>
2024-06-22 09:13:08 +0100

    First commit

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {currentUser} from "@clerk/nextjs";
+// import {Plan} from "@prisma/client";
+
+const Page = () => {
+  return <div>Agency</div>;
+};
+
+export default Page;
