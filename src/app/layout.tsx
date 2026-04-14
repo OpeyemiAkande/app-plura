4017f9557a5993bdd8ee166b4c8dce099f9413b6
Opeyemi Akande <akande212@gmail.com>
2026-04-14 18:09:58 +0100

    fixed the global css issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type {Metadata} from "next";
 import {DM_Sans} from "next/font/google";
-import "./global.css";
+import "./globals.css";
 import {ThemeProvider} from "@/providers/theme-provider";
 import ModalProvider from "@/providers/modal-provider";
 import {Toaster} from "@/components/ui/toaster";
